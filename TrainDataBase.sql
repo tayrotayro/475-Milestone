f4cf40029d06147098a2f6f55e08c18507dc1efa
Marotti13 <43689538+Marotti13@users.noreply.github.com>
2020-03-27 12:05:02 -0500

    hey

--- TrainDataBase.sql
+++ TrainDataBase.sql
@@ -1,12 +1,13 @@
 DROP TABLE IF EXISTS Trains, Stations, Schedules, Conductors, PassengerSchedule, Passengers;
 
+
 Create Table Trains
 (
     trainID VARCHAR(6) Not Null,
     scheduleID VARCHAR(6), 
     trainType VARCHAR(30), 
-    PRIMARY KEY (trainID),
-    CONSTRAINT FK_constr_1 FOREIGN KEY (scheduleID) REFERENCES Schedules(scheduleID)
+    PRIMARY KEY (trainID)
+   -- CONSTRAINT FK_constr_1 FOREIGN KEY (scheduleID) REFERENCES Schedules(scheduleID)
 );
 
 
@@ -16,29 +17,20 @@ Create Table Stations
     city VARCHAR(40),
     stateAbr VARCHAR(2),
     trainID varchar(6) not null,
-    PRIMARY KEY (stationID),
-    CONSTRAINT FK_constr_2 FOREIGN KEY (trainID) REFERENCES Trains(trainID)
+    PRIMARY KEY (stationID)
+   -- CONSTRAINT FK_constr_2 FOREIGN KEY (trainID) REFERENCES Trains(trainID)
 );
 
-
-Create Table Conductors 
-(
-    conductorID VARCHAR(6) Not Null,
-    name VARCHAR(40) Not Null,
-    age INTEGER,
-    certificationLevel VARCHAR(40),
-    PRIMARY KEY (conductorID),
-);
 
 
 Create Table Passengers 
 (
+    /*CONSTRAINT FK_constr_3 FOREIGN KEY (trainID) REFERENCES Trains(trainID)*/
     passengerID varchar(6) Not Null,
     trainID varchar(6),
-    name varchar(40),
+    fullName varchar(40),
     age INTEGER,
-    PRIMARY KEY(passengerID),
-    CONSTRAINT FK_constr_3 FOREIGN KEY (trainID) REFERENCES Trains(trainID)
+    PRIMARY KEY(passengerID) 
 );
 
 
@@ -46,8 +38,16 @@ Create Table PassengerSchedule
 (
     scheduleID varchar(6) Not Null,
     passengerID varchar(6) Not Null,
-    PRIMARY Key(scheduleID),
-    PRIMARY Key(passengerID)
+    PRIMARY Key(scheduleID,passengerID)
+);
+
+Create Table Conductors 
+(
+    conductorID VARCHAR(6) Not Null,
+    fullName VARCHAR(40) Not Null,
+    age INTEGER,
+    certificationLevel VARCHAR(40),
+    PRIMARY KEY(conductorID)
 );
 
 
@@ -58,15 +58,14 @@ Create Table Schedules
     startLocation varchar(40) Not Null,
     endLocation varchar(40),
     finalDestination varchar(40) Not Null,
-    startTime SmallDateTime,
+    startTime DateTime,
     conductorID varchar(6),
-    passengerID varchar(6),
-    CONSTRAINT FK_constr_4 FOREIGN KEY (trainID) REFERENCES Trains(trainID),
-    CONSTRAINT FK_constr_5 FOREIGN KEY (startLocation) REFERENCES Stations(stationID),
-    CONSTRAINT FK_constr_6 FOREIGN KEY (endLocation) REFERENCES Stations(stationID),
-    CONSTRAINT FK_constr_7 FOREIGN KEY (finalDestination) REFERENCES Stations(stationID),
-    CONSTRAINT FK_constr_8 FOREIGN KEY (conductorID) REFERENCES Conductors(conductorID),
-    CONSTRAINT FK_constr_9 FOREIGN KEY (passengerID) REFERENCES Passengers(passengerID)
+    passengerID varchar(6)
+   -- CONSTRAINT FK_constr_4 FOREIGN KEY (trainID) REFERENCES Trains(trainID),
+   -- CONSTRAINT FK_constr_5 FOREIGN KEY (startLocation) REFERENCES Stations(stationID),
+   -- CONSTRAINT FK_constr_6 FOREIGN KEY (endLocation) REFERENCES Stations(stationID),
+   -- CONSTRAINT FK_constr_7 FOREIGN KEY (finalDestination) REFERENCES Stations(stationID),
+   -- CONSTRAINT FK_constr_8 FOREIGN KEY (conductorID) REFERENCES Conductors(conductorID),
+   -- CONSTRAINT FK_constr_9 FOREIGN KEY (passengerID) REFERENCES Passengers(passengerID)
 
 );
-
