e03e080b4a2f384c273919ef1a1252eaf912be80
Marotti13 <43689538+Marotti13@users.noreply.github.com>
2020-03-27 12:31:10 -0500

    help

--- TrainDataBase.sql
+++ TrainDataBase.sql
@@ -1,4 +1,6 @@
+ALTER TABLE Trains DROP DROP FOREIGN KEY FK_constr_1;
 DROP TABLE IF EXISTS Trains, Stations, Schedules, Conductors, PassengerSchedule, Passengers;
+
 
 
 Create Table PassengerSchedule 
@@ -22,8 +24,8 @@ Create Table Trains
     trainID VARCHAR(6) Not Null,
     scheduleID VARCHAR(6), 
     trainType VARCHAR(30), 
-    PRIMARY KEY (trainID)
-   -- CONSTRAINT FK_constr_1 FOREIGN KEY (scheduleID) REFERENCES Schedules(scheduleID)
+    PRIMARY KEY (trainID),
+    CONSTRAINT FK_constr_1 FOREIGN KEY (scheduleID) REFERENCES Schedules(scheduleID)
 );
 
 
@@ -34,19 +36,19 @@ Create Table Stations
     stateAbr VARCHAR(2),
     trainID varchar(6) not null,
     PRIMARY KEY (stationID)
-   -- CONSTRAINT FK_constr_2 FOREIGN KEY (trainID) REFERENCES Trains(trainID)
+    CONSTRAINT FK_constr_2 FOREIGN KEY (trainID) REFERENCES Trains(trainID)
 );
 
 
 
 Create Table Passengers 
 (
-    /*CONSTRAINT FK_constr_3 FOREIGN KEY (trainID) REFERENCES Trains(trainID)*/
     passengerID varchar(6) Not Null,
     trainID varchar(6),
     fullName varchar(40),
     age INTEGER,
-    PRIMARY KEY(passengerID) 
+    PRIMARY KEY(passengerID),
+    CONSTRAINT FK_constr_3 FOREIGN KEY (trainID) REFERENCES Trains(trainID)
 );
 
 
@@ -60,8 +62,8 @@ Create Table Schedules
     finalDestination varchar(40) Not Null,
     startTime DateTime,
     conductorID varchar(6),
-    passengerID varchar(6)
-   -- CONSTRAINT FK_constr_4 FOREIGN KEY (trainID) REFERENCES Trains(trainID),
+    passengerID varchar(6),
+   CONSTRAINT FK_constr_4 FOREIGN KEY (trainID) REFERENCES Trains(trainID)
    -- CONSTRAINT FK_constr_5 FOREIGN KEY (startLocation) REFERENCES Stations(stationID),
    -- CONSTRAINT FK_constr_6 FOREIGN KEY (endLocation) REFERENCES Stations(stationID),
    -- CONSTRAINT FK_constr_7 FOREIGN KEY (finalDestination) REFERENCES Stations(stationID),
